4d67c27bc1813bd2108532c88ed11592c9fbfe0f
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-10-06 15:28:33 +0200

    fixed joinby name - Martin

--- src/main/java/entities/Phone.java
+++ src/main/java/entities/Phone.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entities;
 
 import java.io.Serializable;
@@ -32,7 +27,7 @@ public class Phone implements Serializable {
     private int number;
     private String description;
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "person_id")
+    @JoinColumn(name = "personID")
     private Person person;
 
     public int getNumber() {
