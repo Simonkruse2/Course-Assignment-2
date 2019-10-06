774cb8f088ef884561938e8d2ce4c1128055d84a
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2019-10-06 11:42:21 +0200

    Added Address and CityInto entity classes - Frederik

---
+++ src/main/java/entities/CityInfo.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author frede
+ */
+@Entity
+public class CityInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private int zipCode;
+    private String city;
+    @OneToMany(
+            mappedBy = "address"
+            ,cascade = CascadeType.PERSIST
+//            ,orphanRemoval = true
+    )
+    private ArrayList<Address> addresses;
+
+    public CityInfo(int zipCode, String city) {
+        this.zipCode = zipCode;
+        this.city = city;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public ArrayList<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void addAddresses(Address a) {
+        this.addresses.add(a);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+}
